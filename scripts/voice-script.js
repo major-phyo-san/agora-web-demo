04799de992893d5e8aa84322366843475168f4e9
major-phyo-san <phyojupiter9@gamil.com>
2020-12-18 20:59:30 +0630

    added voice call script

---
+++ scripts/voice-script.js
@@ -0,0 +1,55 @@
+let handleError = function(err){
+    console.log('error', err);
+};
+
+let remoteContrainer = document.getElementById("remote-container");
+
+function addVideoStream(elementId){
+    let streamDiv = document.createElement("div");
+    streamDiv.id = elementId;
+    streamDiv.style.transform = "rotateY(180deg)";
+    remoteContrainer.appendChild(streamDiv);
+}
+
+function removeVideoStream(elementId){
+    let remoteDiv = document.getElementById(elementId);
+    if (remoteDiv) remoteDiv.parentNode.removeChild(remoteDiv);
+}
+
+let client = AgoraRTC.createClient({
+    mode: "rtc",
+    codec: "vp8"
+});
+
+client.init("609dada4ba084fba95bd90bd68b67a86");
+
+client.join("006609dada4ba084fba95bd90bd68b67a86IADo8sJ3/RNPR8CMF2FovnpwrRolEeMMJ4lyhaheRMPaFEOQEggAAAAAEABPLhaFctHdXwEAAQBy0d1f", 
+"myChannel", null, function(uid){
+    let localStream = AgoraRTC.createStream({
+        audio: true,
+        video: false
+    });
+
+    localStream.init(function(){
+        localStream.play("me");
+        client.publish(localStream, handleError);
+    });
+}, handleError);
+
+client.on("stream-added", function(evt){
+    client.subscribe(evt.stream, handleError);
+});
+
+client.on("stream-subscribed", function(evt){
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    addVideoStream(streamId);
+    stream.play(streamId);
+});
+
+client.on("peer-leave", function(evt){
+    let stream = evt.stream;
+    let streamId = String(stream.getId());
+    stream.close();
+    removeVideoStream(streamId);
+});
